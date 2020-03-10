29f575a4a9d4584732e310f786a4b7bfc3407f23
shuiping <37398243+shuipingyang-14@users.noreply.github.com>
2020-03-10 09:52:33 +0800

    获取页面时间

--- helloworld/hello/views.py
+++ helloworld/hello/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def index(request):
@@ -14,3 +14,9 @@ def page(request, num):
         return render(request, 'demo.html')
     except:
         raise Http404
+
+def home(request, year="2018", month="01"):
+    return HttpResponse("获取当前页面home时间标签：%s年/%s月" %(year, month))
+
+def home1(request, year="2018", month="01"):
+    return HttpResponse("获取当前页面home1时间标签：%s年/%s月" %(year, month))
